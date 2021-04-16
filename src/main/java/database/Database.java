2436cab70c7d65c666630644f38fae5791d2ba0e
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-04-16 09:54:21 +0200

    Added base methods and finished getAllTasks

--- src/main/java/database/Database.java
+++ src/main/java/database/Database.java
@@ -1,11 +1,18 @@
 package database;
 
-import com.google.gson.Gson;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import entity.Task;
 import org.bson.Document;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 
 public class Database {
@@ -14,6 +21,61 @@ public class Database {
     private MongoCollection<Document> collection = database.getCollection("tasks");
 
     public void insertTask(Task task){
-        collection.insertOne(new Document("task", new Gson().toJson(task)));
+        Document doc = new Document();
+        doc.put("name", task.getName());
+        doc.put("date", task.getDate());
+        doc.put("priority", task.getPriority());
+        doc.put("done", task.isDone());
+        if (task.getPrice() > 0)
+            doc.put("price", task.getPrice());
+        collection.insertOne(doc);
     }
+
+    public void completeTask(int id){
+
+    }
+
+    public List<Task> getAllTasks(){
+        List<Task> list = new ArrayList<>();
+        for (Document doc : collection.find()){
+            try {
+                JSONObject object = (JSONObject) new JSONParser().parse(doc.toJson());
+                String name = (String) object.get("name");
+                String date = (String) object.get("date");
+                int priority = Integer.parseInt(String.valueOf(object.get("priority")));
+                boolean done = (boolean) object.get("done");
+                Task task = new Task(name, date, priority, done);
+                if (object.get("price") != null)
+                    task.setPrice(Double.parseDouble(String.valueOf(object.get("price"))));
+                list.add(task);
+            } catch (Exception e) { e.printStackTrace(); }
+        }
+        return list;
+    }
+
+    public List<Task> getAllTasks(boolean done){
+        List<Task> list = new ArrayList<>();
+
+
+        return list;
+    }
+
+    public List<Task> getTasksByPriority(){
+        List<Task> list = new ArrayList<>();
+
+
+        return list;
+    }
+
+    public List<Task> getTasksByName(String name){
+        List<Task> list = new ArrayList<>();
+
+
+        return list;
+    }
+
+    public void deleteDoneTasks(){
+
+    }
+
 }
