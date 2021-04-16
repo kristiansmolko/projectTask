170c3a5ef2c1cef2a2aa63aabfff43a580269ec5
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-04-16 09:53:45 +0200

    Added getters and setter

--- src/main/java/entity/Task.java
+++ src/main/java/entity/Task.java
@@ -1,5 +1,6 @@
 package entity;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Task {
@@ -7,17 +8,41 @@ public class Task {
     private double price;
     private int priority;
     private boolean done;
-    private Date date;
+    private String date;
 
-    public Task(String name, double price, int priority, boolean done) {
+    public Task(String name, String date, int priority, boolean done) {
         this.name = name;
-        this.price = price;
+        this.date = date;
         this.priority = priority;
         this.done = done;
     }
 
-    public Task(String name, Date date, double price, int priority, boolean done) {
-        this(name, price, priority, done);
-        this.date = date;
+    public Task(String name, String date, int priority, boolean done, double price) {
+        this(name, date, priority, done);
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
     }
 }
