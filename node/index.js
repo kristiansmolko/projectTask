7dace4a047ce180b3bf6850e0c322e8e0084bc39
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-04-23 09:34:22 +0200

    Added insert task

--- node/index.js
+++ node/index.js
@@ -8,6 +8,14 @@ const mongoClient = mongodb.MongoClient;
 
 var url = "mongodb://localhost:27017/";
 
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+);
+
+app.use(express.json());
+
 function connect(req, res, query) {
     mongoClient.connect(url, function(err, db) {
         if (err) throw err;
@@ -18,6 +26,31 @@ function connect(req, res, query) {
         })
         db.close();
     })
+}
+
+function insert(req, res, doc) {
+    mongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("myFirstDb");
+        dbo.collection("tasks").insertOne(doc, (err, result) => {
+            if (err) throw err;
+            console.log("Document inserted");
+            db.close();
+        })
+    })
+}
+
+function getDate() {
+    var today = new Date();
+    let year = today.getFullYear();
+    let month = ("0" + (today.getMonth() + 1)).slice(-2);
+    let day = ("0" + today.getDate()).slice(-2);
+    let hours = ("0" + today.getHours()).slice(-2);
+    let minutes = ("0" + today.getMinutes()).slice(-2);
+    let seconds = ("0" + today.getSeconds()).slice(-2);
+    var result = year + "-" + month + "-" + day + " " 
+    + hours + ":" + minutes + ":" + seconds
+    return result;
 }
 
 app.get('', (req, res) => {
@@ -72,6 +105,25 @@ app.get('/task', (req, res) => {
         //     })
         // })
     }
+})
+
+app.post('/task', (req, res) => {
+    if (!req.body.task && !req.body.done && !req.body.priority) throw new Error("Wrong values");
+    let date = getDate();
+    var name = req.body.task;
+    var prior = parseInt(req.body.priority);
+    var done = req.body.done=="true"?true:false;
+    var price = parseFloat(req.body.price);
+    var doc = {
+        name: name,
+        priority: prior,
+        date: date,
+        done: done,
+    };
+    if (price > 0) doc.price = price;
+    console.log(date);
+    insert(req, res, doc);
+    res.send("Document inserted");
 })
 
 app.listen(3000, () => {
