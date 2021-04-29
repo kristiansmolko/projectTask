ddc298a90b0fce0111eee5a5b4aa14f9d4d60b85
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-04-29 12:48:42 +0200

    Modified label

--- node/js/task.js
+++ node/js/task.js
@@ -18,11 +18,11 @@ $(() => {
                     const date = result[index].date;
                     const done = result[index].done;
                     const price = result[index].price;
-                    var text = "<label>" + name + " (" + date + ") " + "<br>";
+                    var text = "<label class=\"task_text\"> <b>" + name + "</b> <u>(" + date + ")</u> ";
                     text = text + "Priority: " + priority + "<br>";
                     if (result[index].price) text = text + "Price: " + price + "<br>";
                     text = text + "Done: " + done + "<br>";
-                    text = text + "---------------------------------<br> </label>";
+                    text = text + "</label>";
                     $("#parent").append(text);
                 }
             },
